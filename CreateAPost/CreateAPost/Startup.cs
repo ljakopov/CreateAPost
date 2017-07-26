908c8724aec4806a9d71692a4a15ab7e595dc0ff
Lovro <ljakopov@foi.hr>
2017-07-26 21:17:11 +0200

    First commit

---
+++ CreateAPost/CreateAPost/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CreateAPost.Startup))]
+namespace CreateAPost
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
