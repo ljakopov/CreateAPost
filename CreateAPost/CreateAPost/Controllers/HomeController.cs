43c3dfc36b14e6ef98c1d2f2cd9b8a87ac7173e8
Lovro <ljakopov@foi.hr>
2017-07-28 14:59:34 +0200

    Design for posts are added

--- CreateAPost/CreateAPost/Controllers/HomeController.cs
+++ CreateAPost/CreateAPost/Controllers/HomeController.cs
@@ -1,16 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CreateAPost.Models;
+using CreateAPost.ViewModel;
+using Microsoft.AspNet.Identity;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace CreateAPost.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var userId = User.Identity.GetUserId();
+            var posts = _context.Posts
+                .Where(p => p.UserId == userId)
+                .Include(p => p.User)
+                .ToList();
+
+            var viewModel = new PostViewModel()
+            {
+                UsersPosts = posts,
+                UserAuth = User.Identity.IsAuthenticated
+            };
+            return View(viewModel);
         }
 
         public ActionResult About()
---
+++ CreateAPost/CreateAPost/ViewModel/PostViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CreateAPost.Models;
+using System.Collections.Generic;
+
+namespace CreateAPost.ViewModel
+{
+    public class PostViewModel
+    {
+        public IEnumerable<Post> UsersPosts { get; set; }
+
+        public bool UserAuth { get; set; }
+    }
+}
