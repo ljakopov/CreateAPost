376e32109b862e6b68b679a5ea1b7a002d9fbb65
Lovro <ljakopov@foi.hr>
2017-08-03 01:59:56 +0200

    Desing for added frend. API is added, but no ready

---
+++ CreateAPost/CreateAPost/Controllers/FriendshipController.cs
@@ -0,0 +1,21 @@
+﻿using CreateAPost.Models;
+using System.Web.Http;
+
+namespace CreateAPost.Controllers
+{
+    public class FriendshipController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public FriendshipController()
+        {
+            _context=new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult FriendshipAdd()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ CreateAPost/CreateAPost/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using CreateAPost.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace CreateAPost.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public UsersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var notFriends = _context.Users.Where(u => u.Id != userId).ToList();
+
+            return View(notFriends);
+        }
+    }
+}
